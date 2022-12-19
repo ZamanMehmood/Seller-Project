08a7839ec2031e2e04229d563e1a3d6d50b17c9a
Zaman2031 <zaman342003@gmail.com>
2022-12-20 01:46:13 +0500

    Save and create button of categories page workin

--- src/Components/Category/renderCard.js
+++ src/Components/Category/renderCard.js
@@ -1,3 +1,82 @@
+// import { useState } from "react";
+// import { Card, Button } from "react-bootstrap";
+// import { IconChevronRight } from '@tabler/icons';
+// import Modal from 'react-bootstrap/Modal';
+// import PerfectScrollbar from 'react-perfect-scrollbar';
+// import 'react-perfect-scrollbar/dist/css/styles.css';
+// import s from './index.module.css'
+
+// const RenderCard = ({ list }) => {
+//     const [item, setItem] = useState({});
+//     const [isInput, setIsInput] = useState(false);
+//     const [showModal, setShowModal] = useState(false);
+//     console.log('item----->', item);
+//     const { index } = item;
+//     // handle delete
+//     const handleDelete = () => {
+//         list.splice(index, 1);
+//         setItem(list);
+//         console.log('item ', item.el, 'index ', index)
+//     }
+//     // console.log('list', list)
+
+//     return (
+//         <Card className={s.categoryCard}>
+//             <div className="d-flex justify-content-between">
+//                 <div>
+//                     <button onClick={() => setIsInput(true)} className="btn btn-primary btn-sm">Create New</button>
+//                 </div>
+//                 <div>
+//                     <button onClick={() => setShowModal(true)} className="btn btn-outline-danger btn-sm">Delete</button>
+//                 </div>
+//             </div>
+//             {/* Delete MOdal */}
+//             <Modal backdrop="static" keyboard={false} show={index && showModal} onHide={() => setShowModal(false)}>
+//                 <Modal.Header closeButton>
+//                     <Modal.Title>Delete Item</Modal.Title>
+//                 </Modal.Header>
+//                 <Modal.Body>
+//                     <p>Are you sure, want to delete item?</p>
+//                     <p>{item?.el?.name}</p>
+//                 </Modal.Body>
+//                 <Modal.Footer>
+//                     <Button variant="secondary" onClick={() => setShowModal(false)}>
+//                         Close
+//                     </Button>
+//                     <Button variant="danger" size="samll" onClick={handleDelete}>
+//                         Delete
+//                     </Button>
+//                 </Modal.Footer>
+//             </Modal>
+//             {/* List */}
+//             <div>
+//                 <PerfectScrollbar
+//                     component="div"
+//                     style={{
+//                         height: '510px',
+//                     }}
+//                 >
+//                     <ul className={s.ulStyles}>
+//                         {list && list.length > 0 && list.map((el, index) => (
+//                             <li onClick={() => setItem({ el, index })} key={index} className={(item.index === index) ? `d-flex justify-content-between ${s.listStylesActive}` : `d-flex justify-content-between ${s.listStyles}`}>
+//                                 < div> {el.name}</div>
+//                                 <div><IconChevronRight className={(item.index === index) ? "" : s.showIcon} stroke={1.5} size="1.2rem" /></div>
+//                             </li>
+//                         ))}
+//                     </ul>
+//                 </PerfectScrollbar>
+//             </div >
+//             {isInput &&
+//                 <div className='d-flex justify-content-between pb-2'>
+//                     <div><input name="name" className='form-control' placeholder='Type here...' /></div>
+//                     <div className='pt-1'><button className='btn btn-primary btn-sm'>Save</button></div>
+//                 </div>
+//             }
+//         </Card >
+//     );
+// }
+// export default RenderCard;
+
 import { useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { IconChevronRight } from '@tabler/icons';
@@ -5,18 +84,65 @@ import Modal from 'react-bootstrap/Modal';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import s from './index.module.css'
+import axios from "axios";
 
 const RenderCard = ({ list }) => {
     const [item, setItem] = useState({});
     const [isInput, setIsInput] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    //    state for main categories section , input value
+       const [inputValue, setInputValue] = useState("");
+
+
+   console.log("inputValue", inputValue)
+   
     console.log('item----->', item);
     const { index } = item;
     // handle delete
-    const handleDelete = () => {
-        list.splice(index, 1);
-        setItem(list);
-        console.log('item ', item.el, 'index ', index)
+
+      
+     const keypress = async(e) => {
+        if (e.key === 'Enter') {
+                console.log('do validate');
+
+                let payload = {
+                    Name: inputValue,
+                  }; 
+                  
+                  const apiCall = await axios.post(
+                    "http://localhost:8081/categories/createCategories",payload
+                  );
+                  console.log("apiCall on press Enter", apiCall);
+        } 
+      };
+
+        const handleSave = async (e) => {
+            e.preventDefault();
+            console.log("this is handel save function")
+             
+            console.log("input", inputValue )
+             
+        
+            let payload = {
+              Name: inputValue,
+            }; 
+            
+            const apiCall = await axios.post(
+              "http://localhost:8081/categories/createCategories",payload
+            );
+            console.log("apiCall on save button", apiCall);
+          }
+   
+    const handleDelete = async (e) => {
+        console.log("This is handle delete function")
+     
+         
+    //    const data = await axios.delete(`http://localhost:8081/categories/${payload}`);
+    //      console.log("delete category",data)
+
+    //     // list.splice(index, 1);
+    //     // setItem(list);
+    //     // console.log('item ', item.el, 'index ', index)
     }
     // console.log('list', list)
 
@@ -37,13 +163,13 @@ const RenderCard = ({ list }) => {
                 </Modal.Header>
                 <Modal.Body>
                     <p>Are you sure, want to delete item?</p>
-                    <p>{item?.el?.name}</p>
+                    <p>{item?.el?.Name}</p>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={() => setShowModal(false)}>
                         Close
                     </Button>
-                    <Button variant="danger" size="samll" onClick={handleDelete}>
+                    <Button variant="danger" size="samll" onClick={handleDelete} >
                         Delete
                     </Button>
                 </Modal.Footer>
@@ -59,7 +185,7 @@ const RenderCard = ({ list }) => {
                     <ul className={s.ulStyles}>
                         {list && list.length > 0 && list.map((el, index) => (
                             <li onClick={() => setItem({ el, index })} key={index} className={(item.index === index) ? `d-flex justify-content-between ${s.listStylesActive}` : `d-flex justify-content-between ${s.listStyles}`}>
-                                < div> {el.name}</div>
+                                < div> {el.Name}</div>
                                 <div><IconChevronRight className={(item.index === index) ? "" : s.showIcon} stroke={1.5} size="1.2rem" /></div>
                             </li>
                         ))}
@@ -68,11 +194,25 @@ const RenderCard = ({ list }) => {
             </div >
             {isInput &&
                 <div className='d-flex justify-content-between pb-2'>
-                    <div><input name="name" className='form-control' placeholder='Type here...' /></div>
-                    <div className='pt-1'><button className='btn btn-primary btn-sm'>Save</button></div>
+                    <div>
+                        <input
+                         name="Name" 
+                         className='form-control'
+                         value={inputValue}
+                         onChange={(e) => setInputValue(e.target.value)}
+                         placeholder='Type here...'
+                         onKeyUp={keypress}
+                         
+                          />
+                        </div>
+                    <div className='pt-1'>
+                        <button onClick ={handleSave}  type='submit' className='btn btn-primary btn-sm'>Save</button>
+                        </div>
                 </div>
             }
         </Card >
     );
 }
 export default RenderCard;
+
+ 
